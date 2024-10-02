eb81e8c7d9259f5bec0722b7fe5574ce1a06e6b9
[Mohan Abhijeeth] <[mohanabhi826@gmail.com]>
2024-10-02 16:44:07 +0530

    Initial commit with HTML, CSS, and JS files

---
+++ mathUtil.js
@@ -0,0 +1,40 @@
+export function Sum(){
+    return a+b;
+}
+
+export function Difference(a,b){
+    return a-b;
+}
+
+export function Multiplication(a,b){
+    return a*b;
+}
+
+export function Division(a,b){
+    return a/b;
+}
+
+export function Modulo(a,b){
+    return a%b;
+}
+
+export function Square(a){
+    return Math.pow(a,2);
+}
+
+export function Cube(a){
+    return Math.pow(a,3);
+}
+
+export function squareRoot(a){
+    return Math.sqrt(a);
+}
+
+export function reciprocal(a){
+    return 1/a;
+}
+
+export function power(a,b){
+    return Math.pow(a,b);
+}
+
